cc8ce59998b772eb3d9fce5b21f569104cf651b2
AniaKB <aniakbennett@gmail.com>
2022-11-28 17:11:52 -0500

    adding objects for English and Korean Nav bar

---
+++ script.js
@@ -0,0 +1,13 @@
+const english = {
+  gallery: "Gallery",
+  reels: "Reels",
+  wiki: "Asian Wiki",
+  contact: "Contact"
+}
+
+const korean = {
+  gallery: "갤러리",
+  reels: "릴",
+  wiki: "아시아 위키",
+  contact: "연락하다"
+}
