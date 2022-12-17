b977d72bd776068e3475e3df5e914cc9f92d85b9
AniaKB <aniakbennett@gmail.com>
2022-12-16 22:56:29 -0500

    adding articles to korean version

--- script.js
+++ script.js
@@ -9,5 +9,6 @@ const korean = {
   gallery: "갤러리",
   reels: "릴",
   wiki: "아시아 위키",
-  contact: "연락하다"
+  contact: "연락하다",
+  articles:['https://newsen.com/news_view.php?uid=202210041340251910','https://discord.com/channels/740679561007726702/1018308011179126886/1048842206116581477']
 }
